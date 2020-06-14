b1ef590d0cb01620060ca3835c7524baaa73f40d
Alex Harper <aharper@ecstuning.com>
2020-06-14 18:08:27 -0400

    add token required api request to secret page

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import express from "express";
 // Create express router
 const router = express.Router();
 
+const TOKEN = "example-token"
+
 // Transform req & res to have the same API as express
 // So we can use res.status() & res.json()
 const app = express();
@@ -18,10 +20,20 @@ router.use((req, res, next) => {
 router.post("/login", (req, res) => {
   if (req.body.username === "demo" && req.body.password === "demo") {
     return res.json({
-      token: "testing123"
+      token: TOKEN
     });
   }
   res.status(401).json({ message: "Bad credentials" });
+});
+
+router.get("/secretData", (req, res) => {
+  const headerValue = req.get('Authorization');
+  if (headerValue && headerValue === ('Bearer ' + TOKEN)) {
+    return res.json({
+      message: "secret data here"
+    });
+  }
+  res.status(401).json({ message: "Token Invalid" });
 });
 
 // Export the server middleware
--- pages/secret.vue
+++ pages/secret.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <h1>Super secret page</h1>
+    <h1>Super Secret Page</h1>
+    <p>Secret Message: {{ message }}</p>
     <NuxtLink to="/">Back to the home page</NuxtLink>
   </div>
 </template>
@@ -8,6 +9,19 @@
 <script>
 export default {
   middleware: ['auth'],
-  auth: 'local'
+  auth: 'local',
+  data () {
+    return {
+      message: null,
+    }
+  },
+  async fetch () {
+    try {
+      const result = await this.$axios.$get('/api/secretData')
+      this.message = result.message
+    } catch (e) {
+      this.message = "Error: " + e.message
+    }
+  }
 };
 </script>
